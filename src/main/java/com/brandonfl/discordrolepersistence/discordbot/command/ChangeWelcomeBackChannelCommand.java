f173de0b0498944014e9c8c8e42b1e8ea4a2cb0c
Fontany--Legall Brandon <brandon@fontany-legall.xyz>
2021-01-24 16:41:38 +0100

    clean welcome back command

--- src/main/java/com/brandonfl/discordrolepersistence/discordbot/command/ChangeWelcomeBackChannelCommand.java
+++ src/main/java/com/brandonfl/discordrolepersistence/discordbot/command/ChangeWelcomeBackChannelCommand.java
@@ -2,11 +2,9 @@ package com.brandonfl.discordrolepersistence.discordbot.command;
 
 import com.brandonfl.discordrolepersistence.db.entity.ServerEntity;
 import com.brandonfl.discordrolepersistence.db.repository.RepositoryContainer;
-import com.brandonfl.discordrolepersistence.utils.DiscordBotUtils;
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
 import com.jagrosh.jdautilities.doc.standard.CommandInfo;
-import java.util.Optional;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import net.dv8tion.jda.api.Permission;
@@ -27,34 +25,33 @@ public class ChangeWelcomeBackChannelCommand extends Command {
   @Override
   protected void execute(CommandEvent event) {
     Message msg = event.getMessage();
-    if (DiscordBotUtils.verifyCommandFormat(msg, getName())) {
-      Optional<ServerEntity> possibleServerEntity = repositoryContainer.getServerRepository()
-          .findByGuid(event.getGuild().getIdLong());
-      if (possibleServerEntity.isPresent() && DiscordBotUtils.verifyCommand(possibleServerEntity.get(), msg, getName())) {
-        if (event.getMember() != null && event.getMember().hasPermission(Permission.ADMINISTRATOR)) {
-          if (msg.getMentionedChannels().size() == 1) {
-            if (msg.getMentionedChannels().get(0).canTalk()) {
-              ServerEntity serverEntityToUpdate = possibleServerEntity.get();
-              serverEntityToUpdate.setWelcomeBackChannel(msg.getMentionedChannels().get(0).getIdLong());
-              repositoryContainer.getServerRepository().save(serverEntityToUpdate);
+    if (event.getMember() != null && event.getMember().hasPermission(Permission.ADMINISTRATOR)) {
+      ServerEntity serverEntity = repositoryContainer.getServerRepository()
+          .findByGuid(event.getGuild().getIdLong()).orElse(null);
+      if (serverEntity != null) {
+        if (msg.getMentionedChannels().size() == 1) {
+          if (msg.getMentionedChannels().get(0).canTalk()) {
+            serverEntity.setWelcomeBackChannel(msg.getMentionedChannels().get(0).getIdLong());
+            repositoryContainer.getServerRepository().save(serverEntity);
 
-              event.getChannel().sendMessage(":white_check_mark: Welcome back channel has been changed").queue();
-            } else {
-              event.getChannel().sendMessage(":x: It seems that the bot dont have talk access to this channel").queue();
-            }
-          } else if (DiscordBotUtils.verifyCommand(possibleServerEntity.get(), msg, getName() + " disable")){
-            ServerEntity serverEntityToUpdate = possibleServerEntity.get();
-            serverEntityToUpdate.setWelcomeBackChannel(null);
-            repositoryContainer.getServerRepository().save(serverEntityToUpdate);
-
-            event.getChannel().sendMessage(":white_check_mark: Welcome back channel has been disabled").queue();
+            event.replySuccess("Welcome back channel has been changed");
           } else {
-            event.getChannel().sendMessage(":x: Please provide one and exactly only one channel ").queue();
+            event.replyError("It seems that the bot dont have talk access to this channel");
           }
+        } else if (event.getArgs().equals("disable")) {
+          serverEntity.setWelcomeBackChannel(null);
+          repositoryContainer.getServerRepository().save(serverEntity);
+
+          event.replySuccess("Welcome back channel has been disabled");
         } else {
-          event.getChannel().sendMessage(":octagonal_sign: Only administrators can perform this action").queue();
+          event.replyError("Please provide one and exactly only one channel ");
         }
+      } else {
+        event.replyWarning("Current server not found");
       }
+    } else {
+      event.getChannel().sendMessage(":octagonal_sign: Only administrators can perform this action")
+          .queue();
     }
   }
 }
