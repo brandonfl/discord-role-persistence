c6a40083997b6f079162a180f2a5e229387a8092
Fontany--Legall Brandon <brandon@fontany-legall.xyz>
2021-11-21 15:57:56 +0100

    better get roles perm check

--- src/main/java/com/brandonfl/discordrolepersistence/discordbot/command/GetRolesCommand.java
+++ src/main/java/com/brandonfl/discordrolepersistence/discordbot/command/GetRolesCommand.java
@@ -59,53 +59,49 @@ public class GetRolesCommand extends SlashCommand {
 
     this.name = "roles";
     this.help = "Get roles status of the current server.";
+    this.userPermissions = new Permission[]{Permission.ADMINISTRATOR};
   }
 
   @Override
   @Transactional(readOnly = true)
   public void execute(SlashCommandEvent event) {
     event.deferReply().setContent("Retrieving roles...").queue();
-    if (event.getMember() != null && event.getMember().hasPermission(Permission.ADMINISTRATOR)) {
 
-      final Set<Long> serverRoleBlacklistedIds = repositoryContainer.getServerRoleRepository()
-          .findAllByServerGuidId(event.getGuild().getIdLong())
-          .parallelStream()
-          .filter(ServerRoleEntity::isBlacklisted)
-          .map(ServerRoleEntity::getRoleGuid)
-          .collect(Collectors.toSet());
+    final Set<Long> serverRoleBlacklistedIds = repositoryContainer.getServerRoleRepository()
+        .findAllByServerGuidId(event.getGuild().getIdLong())
+        .parallelStream()
+        .filter(ServerRoleEntity::isBlacklisted)
+        .map(ServerRoleEntity::getRoleGuid)
+        .collect(Collectors.toSet());
 
-      final Member currentBotMember = event.getGuild().getSelfMember();
-      List<String> rolesString = new ArrayList<>();
-      for (Role role : event.getGuild().getRoles()) {
-        if (role.isPublicRole()) {
-          continue;
-        } else if (role.isManaged()) {
-          rolesString.add(":robot: " + role.getAsMention() + " (Cannot be assigned manually)");
-        } else if (role.hasPermission(Permission.ADMINISTRATOR)) {
-          rolesString.add(":no_entry: " + role.getAsMention() + " (Administrator role)");
-        } else if (!serverRoleBlacklistedIds.isEmpty()
-            && serverRoleBlacklistedIds.stream()
-            .anyMatch(roleId -> roleId.equals(role.getIdLong()))) {
-          rolesString.add(":lock: " + role.getAsMention() + " (Locked role)");
-        } else if (DiscordBotUtils.getUpperRole(currentBotMember.getRoles()) < role
-            .getPosition()) {
-          rolesString.add(":warning: " + role.getAsMention()
-              + " (bot too low in the hierarchy to give this role)");
-        } else {
-          rolesString.add(":white_check_mark:  " + role.getAsMention());
-        }
+    final Member currentBotMember = event.getGuild().getSelfMember();
+    List<String> rolesString = new ArrayList<>();
+    for (Role role : event.getGuild().getRoles()) {
+      if (role.isPublicRole()) {
+        continue;
+      } else if (role.isManaged()) {
+        rolesString.add(":robot: " + role.getAsMention() + " (Cannot be assigned manually)");
+      } else if (role.hasPermission(Permission.ADMINISTRATOR)) {
+        rolesString.add(":no_entry: " + role.getAsMention() + " (Administrator role)");
+      } else if (!serverRoleBlacklistedIds.isEmpty()
+          && serverRoleBlacklistedIds.stream()
+          .anyMatch(roleId -> roleId.equals(role.getIdLong()))) {
+        rolesString.add(":lock: " + role.getAsMention() + " (Locked role)");
+      } else if (DiscordBotUtils.getUpperRole(currentBotMember.getRoles()) < role
+          .getPosition()) {
+        rolesString.add(":warning: " + role.getAsMention()
+            + " (bot too low in the hierarchy to give this role)");
+      } else {
+        rolesString.add(":white_check_mark:  " + role.getAsMention());
       }
-
-      Paginator.Builder paginatorBuilder = getGenericPaginatorBuilder(eventWaiter);
-      paginatorBuilder.clearItems();
-      rolesString.forEach(paginatorBuilder::addItems);
-      paginatorBuilder
-          .setText("Server roles")
-          .build()
-          .paginate(event.getHook().retrieveOriginal().complete(), 1);
-    } else {
-      event.getChannel().sendMessage(":octagonal_sign: Only administrators can perform this action")
-          .queue();
     }
+
+    Paginator.Builder paginatorBuilder = getGenericPaginatorBuilder(eventWaiter);
+    paginatorBuilder.clearItems();
+    rolesString.forEach(paginatorBuilder::addItems);
+    paginatorBuilder
+        .setText("Server roles")
+        .build()
+        .paginate(event.getHook().retrieveOriginal().complete(), 1);
   }
 }
