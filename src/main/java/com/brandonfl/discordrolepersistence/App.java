81a674733835f0459df0e85202120e4f88b617cc
Fontany--Legall Brandon <brandon@fontany-legall.xyz>
2022-08-07 16:08:30 +0200

    imports clean

--- src/main/java/com/brandonfl/discordrolepersistence/App.java
+++ src/main/java/com/brandonfl/discordrolepersistence/App.java
@@ -32,8 +32,6 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.annotation.PostConstruct;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class App {
