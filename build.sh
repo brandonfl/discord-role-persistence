b798f8f88376f966b17e3f5fb16a96466ea1137f
Fontany--Legall Brandon <brandon@fontany-legall.xyz>
2023-05-07 15:22:58 +0200

    spring-boot:build-image is also not working...

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ docker run \
 -v /var/run/docker.sock:/var/run/docker.sock \
 -v $PWD:/src \
 -w /src \
-buildpacksio/pack build test --builder=paketobuildpacks/builder:full
+maven:latest mvn spring-boot:build-image
